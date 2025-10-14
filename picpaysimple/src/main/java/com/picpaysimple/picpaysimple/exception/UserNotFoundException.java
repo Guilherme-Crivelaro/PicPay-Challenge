7eeab2732ff0ed0954fcde88155b2b94321930a2
Guilherme-Crivelaro <crivelarogui@gmail.com>
2025-10-14 16:33:09 -0300

    feat(app): created exception UnauthorizedTransactionException

---
+++ picpaysimple/src/main/java/com/picpaysimple/picpaysimple/exception/UserNotFoundException.java
@@ -0,0 +1,11 @@
+package com.picpaysimple.picpaysimple.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
