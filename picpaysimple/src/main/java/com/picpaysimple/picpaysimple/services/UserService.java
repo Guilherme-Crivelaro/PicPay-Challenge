505e2680528e07daea4af2fecf5b6e65e2a2cb89
Guilherme-Crivelaro <crivelarogui@gmail.com>
2025-10-15 15:12:31 -0300

    feat(app): add user creation methods and method to show all registered users

--- picpaysimple/src/main/java/com/picpaysimple/picpaysimple/services/UserService.java
+++ picpaysimple/src/main/java/com/picpaysimple/picpaysimple/services/UserService.java
@@ -2,6 +2,7 @@ package com.picpaysimple.picpaysimple.services;
 
 import com.picpaysimple.picpaysimple.domain.user.User;
 import com.picpaysimple.picpaysimple.domain.user.UserType;
+import com.picpaysimple.picpaysimple.dto.UserDTO;
 import com.picpaysimple.picpaysimple.exception.InsufficientBalanceException;
 import com.picpaysimple.picpaysimple.exception.MerchantTransactionNotAllowedException;
 import com.picpaysimple.picpaysimple.exception.UserNotFoundException;
@@ -11,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -31,6 +33,16 @@ public class UserService {
     public User findUserById(Long id){
         return this.userRepository.findUserById(id).orElseThrow(()
                 -> new UserNotFoundException("User not found"));
+    }
+
+    public User createUser(UserDTO userDTO){
+        User newUser = new User(userDTO);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers(){
+        return this.userRepository.findAll();
     }
 
     public void saveUser(User user){
